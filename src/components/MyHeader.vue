5012abfa81117505b497f41d94d8d6687a4b7e9c
saidmoussadek <saiddus24@gmail.com>
2022-08-28 22:56:32 +0200

    inzializzazione progetto

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-light bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar  ABCDEFGHIMNPSQRWXT</a>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    nome:'MyHeader'
+
+}
+</script>
+
+<style>
+
+</style>
