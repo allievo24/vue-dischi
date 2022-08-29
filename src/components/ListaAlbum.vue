4d55d894fa450ac2736defa1d9855710e824dd63
saidmoussadek <saiddus24@gmail.com>
2022-08-29 02:47:02 +0200

    il ciclo non avviene

--- src/components/ListaAlbum.vue
+++ src/components/ListaAlbum.vue
@@ -1,11 +1,39 @@
 <template>
+    <div class="container">
+        <SingolAlbum v-for ="(album, index) in ListaAlbum " :key="index" :album="ListaAlbum"/>
+
+    </div>
   
 </template>
 
 <script>
+import axios from 'axios';
+import SingolAlbum from './SingolAlbum.vue'
 
 export default {
     name: 'ListaAlbum',
+    components:{
+      SingolAlbum
+    },
+    data(){
+        return{
+            ListaAlbum: [],
+            endPoint: 'https://flynn.boolean.careers/exercises/api/array/music'
+        }
+    },
+    created(){
+        axios.get(this.endPoint)
+        .then(response =>{
+            this.ListaAlbum =response.data;
+
+        })
+        .catch(err =>{
+            console.log(err);
+
+        })
+
+        
+    },
 
 }
 </script>
---
+++ src/components/SingolAlbum.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="card">
+    <img :src="album.poster" alt="">
+    <h3>{{album.author}}</h3>-->
+    <h4>NEW JERSY</h4>
+    <span>anno</span>
+</div>
+  
+</template>
+
+<script>
+export default {
+    nome: 'SingolAlbum',
+    props:{
+        album:Object
+       
+    }
+
+}
+</script>
+
+<style>
+
+</style>
